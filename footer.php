c3c4be26372a2e70d6834f33429d49727f599389
Jonathan Roy <jroy@optimumweb.ca>
2017-10-09 16:47:46 -0400

    style

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
             <?php endif; ?>
             <div id="bottom-bar">
                 <div class="<?php wpbp_container_class(); ?>">
-                    <div class="grid_6 mobile-center">
+                    <div class="grid_8 mobile-center">
                         <?php if ( has_nav_menu('secondary_navigation') ) : ?>
                             <nav id="footer-nav">
                                 <?php wp_nav_menu(array( 'theme_location' => 'secondary_navigation' )); ?>
@@ -18,30 +18,24 @@
                             </nav>
                         <?php endif; ?>
                     </div>
-                    <div class="grid_3 mobile-center">
-                        <?php if ( function_exists('of_get_option') ) : ?>
-                            <ul class="social-links">
-                                <?php if ( $twitter_url = of_get_option('twitter_url') ) : ?>
-                                    <li>
-                                        <a href="<?php echo $twitter_url; ?>" target="_blank">
-                                            <i class="fa fa-twitter"></i>
-                                        </a>
-                                    </li>
-                                <?php endif; ?>
-                                <?php if ( $facebook_url = of_get_option('facebook_url') ) : ?>
-                                    <li>
-                                        <a href="<?php echo $facebook_url; ?>" target="_blank">
-                                            <i class="fa fa-facebook"></i>
-                                        </a>
-                                    </li>
-                                <?php endif; ?>
-                            </ul>
-                        <?php endif; ?>
-                    </div>
-                    <div class="grid_3 text-right mobile-center">
+                    <div class="grid_4 text-right mobile-center">
                         <div id="copy">
                             &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
                         </div>
+                        <?php if ( function_exists('of_get_option') ) : ?>
+                            <div id="social-links">
+                                <?php if ( $twitter_url = of_get_option('twitter_url') ) : ?>
+                                    <a href="<?php echo $twitter_url; ?>" target="_blank">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                <?php endif; ?>
+                                <?php if ( $facebook_url = of_get_option('facebook_url') ) : ?>
+                                    <a href="<?php echo $facebook_url; ?>" target="_blank">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
