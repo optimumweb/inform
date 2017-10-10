b7832c7eecd5ffbfc8c103b588fbe84cb9160f2d
Jonathan Roy <jroy@optimumweb.ca>
2017-10-09 21:24:08 -0400

    style

--- related-post.php
+++ related-post.php
@@ -1,28 +1,26 @@
 <?php wpbp_post_before(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('related-post'); ?>>
     <?php wpbp_post_inside_before(); ?>
-    <header class="post-header">
-        <div class="<?php wpbp_container_class(); ?>">
-            <div class="grid_3">
-                <?php if ( has_post_thumbnail() ) : ?>
-                    <div class="post-thumbnail">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('inform_small'); ?>
-                        </a>
-                    </div>
-                <?php endif; ?>
-            </div>
-            <div class="grid_9">
-                <div class="post-meta">
-                    <time class="post-date updated" datetime="<?php the_time('c'); ?>" pubdate><?php the_time(__('l, F jS, Y', 'wpbp')); ?></time> |
-                    <span class="post-author byline author vcard"><?php the_author_posts_link(); ?></span>
+    <div class="<?php wpbp_container_class(); ?>">
+        <div class="grid_3">
+            <?php if ( has_post_thumbnail() ) : ?>
+                <div class="post-thumbnail">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('inform_small'); ?>
+                    </a>
                 </div>
-                <h2 class="post-title">
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h2>
-            </div>
+            <?php endif; ?>
         </div>
-    </header>
+        <div class="grid_9">
+            <div class="post-meta">
+                <time class="post-date updated" datetime="<?php the_time('c'); ?>" pubdate><?php the_time(__('l, F jS, Y', 'wpbp')); ?></time> |
+                <span class="post-author byline author vcard"><?php the_author_posts_link(); ?></span>
+            </div>
+            <h2 class="post-title">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h2>
+        </div>
+    </div>
     <?php wpbp_post_inside_after(); ?>
 </article>
 <?php wpbp_post_after(); ?>
